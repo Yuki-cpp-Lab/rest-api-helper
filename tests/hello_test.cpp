08da51ca66dfb10ace2f0c8fe0a04ace56a1facd
Leo <leo.ghafari+github@gmail.com>
2026-01-22 17:06:04 +0900

    Setup the web-server helper (#5)

--- tests/hello_test.cpp
+++ tests/hello_test.cpp
@@ -1,9 +1,3 @@
-#include "src/hello.hpp"
+
 
 #include "gtest/gtest.h"
-
-TEST(GreeterTest, SayHello)
-{
-    rest_api_helper::Greeter greeter("Bazel");
-    EXPECT_EQ(greeter.say_hello(), "Hello, Bazel!");
-}
